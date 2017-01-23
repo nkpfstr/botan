a79ee5d635632bd85c61cea6ca95699373362dfc
Nick Pfisterer <nick@lunamachine.net>
2017-01-22 18:16:23 -0800

    :construction: Gulp 4.0/Metalsmith refactor
    - Switch to Gulp 4.0 for task management
    - Use Metalsmith to build site pages/content

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,56 @@
 
 // Import dependencies
 const gulp = require('gulp')
-const markdown = require('gulp-markdown')
-const pug = require('gulp-pug')
-const config = require('./config')
+const fs = require('fs-extra')
+const yaml = require('js-yaml')
+const metalsmith = require('metalsmith')
+const collections = require('metalsmith-collections')
+const markdown = require('metalsmith-markdown')
+const permalinks = require('metalsmith-permalinks')
+const layouts = require('metalsmith-layouts')
+const sitemap = require('metalsmith-mapsite')
+const rss = require('metalsmith-feed')
 
-// Turn Markdown into content
-gulp.task('markdown', () => {
-  return gulp.src('_content/**/*.md')
-    .pipe(markdown())
-    .pipe(gulp.dest('./docs'))
-})
+// Import configuration file
+const data = yaml.safeLoad(fs.readFileSync('./botan.yml', 'utf-8'))
 
-// Turn Pug into HTML
-gulp.task('templates', () => {
-  return gulp.src('_templates/**/*.pug')
-     .pipe(pug({
-       data: config,
-       pretty: true
-     }))
-     .pipe(gulp.dest('./docs'))
-})
+// Build site pages and content
+function content (done) {
+  metalsmith(__dirname)
+    .metadata(data) // Add site config to metadata
+    .source('_content') // Process files in this folder
+    .destination('docs') // Output files in this folder
+    .clean(true) // Empty destination folder before building files
+    .use(collections({ // Group related content for easy access later
+      blog: 'blog/*.md',
+      projects: 'projects/*.md'
+    }))
+    .use(markdown({ // Process Markdown files
+      smartypants: true
+    }))
+    .use(permalinks({ // Add pretty permalinks
+      relative: false
+    }))
+    .use(layouts({ // Wrap content with Handlebars templates
+      engine: 'handlebars',
+      default: 'default.hbs',
+      directory: './_templates',
+      partials: './_templates/partials'
+    }))
+    .use(sitemap(data.site.url)) // Generate a sitemap
+    .use(rss({ // Generate RSS feeds for collections
+      collection: 'blog'
+    }))
+    .build(err => {
+      if (err) throw err
+    })
 
-gulp.task('default', ['markdown', 'templates'])
+  // Signal async completion
+  done()
+}
+
+// Define build process
+let botan = gulp.series(content)
+
+// Bingo, bingo, you win the prize!
+gulp.task('default', botan)
